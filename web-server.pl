7e12ebb2dfe07afdacb82f940e0dbdfdd5ef66d7
Kayne Richens <kayno@kayno.net>
2012-09-29 17:18:48 +1000

    fixed issue with log not working on web site

--- web-server.pl
+++ web-server.pl
@@ -4,6 +4,32 @@
     use HTTP::Server::Simple::CGI;
     use base qw(HTTP::Server::Simple::CGI);
     use JSON;
+    use AppConfig;
+
+    our $config = AppConfig->new();
+    $config->define("eth2ser_ip_address=s");
+    $config->define("eth2ser_port=s");
+    $config->define("serial_port=s");
+    $config->define("pvoutput_enabled=s");
+    $config->define("pvoutput_api_key=s");
+    $config->define("pvoutput_system_id=s%");
+    $config->define("pvoutput_status_interval_mins=s");
+    $config->define("pvoutput_add_status_url=s");
+    $config->define("options_debug=s");
+    $config->define("options_query_inverter_secs=s");
+    $config->define("options_log_file=s");
+    $config->define("options_communication_method=s");
+    $config->define("options_strings=s");
+    $config->define("seg_enabled=s");
+    $config->define("seg_upload_interval_mins=s");
+    $config->define("seg_site_id=s");
+    $config->define("seg_device=s");
+    $config->define("seg_power_stream=s");
+    $config->define("seg_energy_stream=s");
+    $config->define("seg_api_url=s");
+    $config->define("web_server_enabled=s");
+    $config->define("web_server_port=s");
+    $config->file("eversolar.ini");
 
     my %dispatch = (
         '/' => \&index,
